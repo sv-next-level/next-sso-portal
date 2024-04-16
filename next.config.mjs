1f6e6f7b3f773a3e817a2627c929e4b59bb04cdc
Sagar-v4 <sagarvariya4@gmail.com>
2024-04-16 23:10:03 +0530

    update: next js version

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    webpack: (config) => {
-      config.resolve.fallback = {
-        "mongodb-client-encryption": false,
-        aws4: false,
-      };
-  
-      return config;
-    },};
+  webpack: (config) => {
+    config.resolve.fallback = {
+      "mongodb-client-encryption": false,
+      aws4: false,
+    };
+
+    return config;
+  },
+  experimental: {
+    turbo: {
+      rules: {
+        "*.svg": {
+          loaders: ["@svgr/webpack"],
+          as: "*.js",
+        },
+      },
+    },
+  },
+};
 
 export default nextConfig;
